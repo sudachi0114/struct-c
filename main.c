b77dbcbf0cfbb30a6b92448bb6b980ad65220b76
sudachi0114 <jnevariste@gmail.com>
2022-12-31 17:52:23 +0900

    define int-type Pointer type

--- main.c
+++ main.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
-typedef int seisu_t;
+// intPtr_t: 「int型を表すポインタ」型
+typedef int* intPtr_t;
+
+intPtr_t foo(intPtr_t p) {
+    return p + 3;
+}
+
 
 int main(void) {
-    int a = 0;
-    seisu_t b = 1;
+    int a[10];
 
-    printf("a = %d, b = %d\n", a, b);
+    // 配列 a の初期化
+    for (int i=0; i<10; i++) {
+        a[i] = i;
+        // printf("a[%d]: %d\n", i, a[i]);
+    }
+
+    intPtr_t p;
+    p = foo(a);
+
+    printf("*p = %d\n", *p);
 
     return 0;
 }
